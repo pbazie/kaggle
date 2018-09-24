8f350a8205996429ffc3b4307a244677cbf47966
yacouba-kone <40605115+yacouba-kone@users.noreply.github.com>
2018-09-24 16:39:42 +0100

    Ajout de membre
    Co-Authored-By: pbazie <pbazie@users.noreply.github.com>

---
+++ Programmes&algorithme/Data_cleaning.R
@@ -0,0 +1,8 @@
+#############################################################
+#
+# Part : Data cleaning
+# 
+# Autor : Yacouba.K
+############################################################
+
+exemple<-read.csv("F:/Semestre 3/Challenge Kaggle & Big Data/Projet_Kaggle/Data de base/RequetesGoogleParRegion/Alsace.csv",header = T, sep=",")
